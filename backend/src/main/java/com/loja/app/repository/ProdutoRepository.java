ad3af60d44c16afebd7a293c723d70b7b80a5be0
Jadilson Guedes <jadilson12@gmail.com>
2019-09-22 08:36:58 -0300

    [ADD] Adiciona Optional para não aceita nome da categoria repetido

--- backend/src/main/java/com/loja/app/repository/ProdutoRepository.java
+++ backend/src/main/java/com/loja/app/repository/ProdutoRepository.java
@@ -3,5 +3,8 @@ package com.loja.app.repository;
 import com.loja.app.models.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProdutoRepository  extends JpaRepository<Produto, Long> {
+    Optional<Produto> findProdutoByCategoria(String categoria);
 }
