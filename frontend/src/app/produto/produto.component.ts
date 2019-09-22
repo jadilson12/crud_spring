e934a375aafbe782c150dbf1e2b5ce9d98dcdc49
Jadilson Guedes <jadilson12@gmail.com>
2019-09-22 16:49:48 -0300

    [ADD] Adicionar component produto com a tabela listar

---
+++ frontend/src/app/produto/produto.component.ts
@@ -0,0 +1,27 @@
+import { ProdutoService } from './../produto.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-produto',
+  templateUrl: './produto.component.html',
+  styleUrls: ['./produto.component.css']
+})
+export class ProdutoComponent implements OnInit {
+
+  produto = {};
+  produtos = [];
+  button = 'Adicionar';
+  constructor(
+  private ProdutoServices: ProdutoService,
+  ) { }
+
+  ngOnInit() {
+    this.list();
+  }
+
+  list() {
+    this.ProdutoServices.getProduto()
+      .subscribe(response => this.produtos = <any> response);
+  }
+
+}
