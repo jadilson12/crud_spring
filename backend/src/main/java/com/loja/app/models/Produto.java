ab5b9c310c1d38deb50e26f57e3fdf1a859d1b2b
Jadilson Guedes <jadilson12@gmail.com>
2019-09-21 23:37:31 -0300

    [ADD] Model Produto

---
+++ backend/src/main/java/com/loja/app/models/Produto.java
@@ -0,0 +1,66 @@
+package com.loja.app.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+public class Produto {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    private String nome;
+
+    @NotEmpty
+    private String categoria;
+
+    @NotEmpty
+    private Integer quantidade;
+
+    private Float preco;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Float preco) {
+        this.preco = preco;
+    }
+}
