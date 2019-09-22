82d6fa364840554fc02bc1aee37536ea0660565f
Jadilson Guedes <jadilson12@gmail.com>
2019-09-22 00:26:54 -0300

    [UPDATE] ProdutoController atualizar o Get todos produtos

--- backend/src/main/java/com/loja/app/controller/ProdutoController.java
+++ backend/src/main/java/com/loja/app/controller/ProdutoController.java
@@ -1,15 +1,26 @@
 package com.loja.app.controller;
 
-import org.springframework.stereotype.Controller;
+import com.loja.app.models.Produto;
+import com.loja.app.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+
+@CrossOrigin // Cors permite todos acessos
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
 
-    public String get() {
-        return  "Listar produtos";
+    @Autowired
+    private ProdutoRepository produtos;
+
+    @GetMapping
+    public List<Produto> get() {
+        return  produtos.findAll();
     }
 }
