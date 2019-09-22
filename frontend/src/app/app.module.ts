05567a5569987fade57a96ca201e3027d98cacc9
Jadilson Guedes <jadilson12@gmail.com>
2019-09-22 16:47:37 -0300

    [ADD] Adiciona dependencias primeNG e bootstrap

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,47 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TemplateComponent } from './template/template.component';
+import { ToolbarComponent } from './template/toolbar/toolbar.component';
+
+import {ToolbarModule} from 'primeng/toolbar';
+import {ButtonModule} from 'primeng/button';
+import {SplitButtonModule} from 'primeng/splitbutton';
+import {MenubarModule} from 'primeng/menubar';
+import {MenuItem} from 'primeng/api';
+import {InputTextModule} from 'primeng/inputtext';
+import {MegaMenuModule} from 'primeng/megamenu';
+import { ProdutoComponent } from './produto/produto.component';
+import {TableModule} from 'primeng/table';
+import { HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateComponent,
+    ToolbarComponent,
+    ProdutoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+
+    BrowserAnimationsModule,
+    ToolbarModule,
+    ButtonModule,
+    SplitButtonModule,
+    MenubarModule,
+    InputTextModule,
+    MegaMenuModule,
+    TableModule
+
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [TemplateComponent]
 })
 export class AppModule { }
