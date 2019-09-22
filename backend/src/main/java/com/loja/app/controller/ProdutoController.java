31ad943f0939bf93d984b3feacfdc9cbd431891f
Jadilson Guedes <jadilson12@gmail.com>
2019-09-21 23:13:54 -0300

    [ADD] ProdutoController com o metodo listar produtos

---
+++ backend/src/main/java/com/loja/app/controller/ProdutoController.java
@@ -0,0 +1,15 @@
+package com.loja.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    public String get() {
+        return  "Listar produtos";
+    }
+}
